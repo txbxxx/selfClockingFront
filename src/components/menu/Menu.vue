96e669e497affd02ca869a63f1bbf438f60cfc60
TanChang <2865260231@qq.com>
2024-05-28 23:12:16 +0800

    修改了登录后界面的菜单栏和日历

---
+++ src/components/menu/Menu.vue
@@ -0,0 +1,95 @@
+<template>
+  <el-row class="tac">
+    <el-col :span="12">
+
+        <el-menu
+            default-active="activeIndex"
+            class="el-menu-vertical-demo"
+            :collapse="props.isCollapse"
+
+            style="height: 100%;"
+            @open="handleOpen"
+            @close="handleClose"
+            @select="handleSelect"
+        >
+          <!--头像-->
+          <el-sub-menu index="2"  >
+            <template #title>
+              <div style="margin: 0 auto;" >
+                <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" shape="square" :size="props.isCollapse?30: 60"/>
+              </div>
+            </template>
+            <el-menu-item index="2-1">退出登录</el-menu-item>
+          </el-sub-menu>
+
+          <el-divider></el-divider>
+
+          <h5 class="mb-2">欢迎</h5>
+
+          <el-sub-menu index="1">
+            <template #title>
+              <el-icon><location /></el-icon>
+              <span>首页</span>
+            </template>
+            <el-menu-item-group title="Group One">
+              <el-menu-item index="1-1">item one</el-menu-item>
+              <el-menu-item index="1-2">item two</el-menu-item>
+            </el-menu-item-group>
+            <el-menu-item-group title="Group Two">
+              <el-menu-item index="1-3">item three</el-menu-item>
+            </el-menu-item-group>
+            <el-sub-menu index="1-4">
+              <template #title>item four</template>
+              <el-menu-item index="1-4-1">item one</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+
+          <el-menu-item index="2">
+            <el-icon><icon-menu /></el-icon>
+            <span>任务表</span>
+          </el-menu-item>
+          <el-menu-item index="3" disabled>
+            <el-icon><document /></el-icon>
+            <span>Navigator Three</span>
+          </el-menu-item>
+          <el-menu-item index="4">
+            <el-icon><setting /></el-icon>
+            <span>Navigator Four</span>
+          </el-menu-item>
+        </el-menu>
+    </el-col>
+  </el-row>
+</template>
+
+<script setup>
+import {ArrowLeft, ArrowRight, Document, Location, Setting} from "@element-plus/icons-vue";
+import {computed, ref} from "vue";
+import UserFunc from "@/hooks";
+
+const activeIndex = ref('1')
+
+// 使用服务逻辑接口
+const { User_logout } = UserFunc()
+
+const props = defineProps({
+  isCollapse: Boolean,
+});
+
+//菜单逻辑匹配
+const handleSelect = (key, keyPath) => {
+  console.log(key)
+  //点击退出登录就调用退出登录
+  if (key === "2-1"){
+    User_logout().then((res)=>{
+      console.log(res)
+    })
+  }
+}
+
+
+</script>
+
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,73 +3,29 @@
     <el-container>
       <el-container  >
           <el-aside    :width="isCollapse?'70px':'200px'"  style="height: 700px;"  class="TaskPageAside">
-            <el-row class="tac">
-              <el-col :span="12">
-                <el-space direction="vertical" :size="300 " >
-                  <el-menu
-                      default-active="2"
-                      class="el-menu-vertical-demo"
-                      :collapse="isCollapse"
+            <el-space direction="vertical" :size="250 " >
 
-                      style="height: 100%;"
-                      @open="handleOpen"
-                      @close="handleClose"
-                  >
-                      <!--头像-->
-                      <el-sub-menu index="2"  >
-                        <template #title>
-                          <div style="margin: 0 auto;" >
-                            <el-avatar src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png" shape="square" :size="isCollapse?30: 60"/>
-                          </div>
-                        </template>
-                        <el-menu-item index="2-1">退出登录</el-menu-item>
-                      </el-sub-menu>
+              <Menu :isCollapse="isCollapse"  />
 
-                    <el-divider></el-divider>
-
-                    <h5 class="mb-2">欢迎</h5>
-
-                    <el-sub-menu index="1">
-                      <template #title>
-                        <el-icon><location /></el-icon>
-                        <span>首页</span>
-                      </template>
-                      <el-menu-item-group title="Group One">
-                        <el-menu-item index="1-1">item one</el-menu-item>
-                        <el-menu-item index="1-2">item two</el-menu-item>
-                      </el-menu-item-group>
-                      <el-menu-item-group title="Group Two">
-                        <el-menu-item index="1-3">item three</el-menu-item>
-                      </el-menu-item-group>
-                      <el-sub-menu index="1-4">
-                        <template #title>item four</template>
-                        <el-menu-item index="1-4-1">item one</el-menu-item>
-                      </el-sub-menu>
-                    </el-sub-menu>
-
-                    <el-menu-item index="2">
-                      <el-icon><icon-menu /></el-icon>
-                      <span>任务表</span>
-                    </el-menu-item>
-                    <el-menu-item index="3" disabled>
-                      <el-icon><document /></el-icon>
-                      <span>Navigator Three</span>
-                    </el-menu-item>
-                    <el-menu-item index="4">
-                      <el-icon><setting /></el-icon>
-                      <span>Navigator Four</span>
-                    </el-menu-item>
-                  </el-menu>
-
-                  <div class="toggle-button" @click="toggleCollapse">
-                    <el-icon v-if="isCollapse"><arrow-right /></el-icon>
-                    <el-icon v-else><arrow-left /></el-icon>
-                  </div>
-                </el-space>
+              <div class="toggle-button" @click="toggleCollapse">
+                <el-icon v-if="isCollapse"><arrow-right /></el-icon>
+                <el-icon v-else><arrow-left /></el-icon>
+              </div>
+            </el-space>
+          </el-aside>
+          <el-main  :class="mainStyle" class="TaskPageMain">
+            <el-row :gutter="10">
+              <el-col :span="7">
+                  打卡区域
+              </el-col>
+              <el-col :span="10">
+                任务区域
+              </el-col>
+              <el-col :span="7">
+                <Clock />
               </el-col>
             </el-row>
-          </el-aside>
-          <el-main  :class="mainStyle" class="TaskPageMain">Main</el-main>
+          </el-main>
       </el-container>
     </el-container>
   </div>
@@ -79,12 +35,12 @@
 
 <script setup>
 // @ is an alias to /src
+import Menu from "../components/menu/Menu.vue";
+import Clock from "../components/clockTask/Clock.vue";
 import Logout from "../components/logout/Logout.vue";
 import {computed, ref} from 'vue'
 import UserFunc from "@/hooks";
 import {ArrowLeft, ArrowRight, Document, Location, Setting} from "@element-plus/icons-vue";
-const { User_logout } = UserFunc()
-const activeIndex = ref('1')
 
 // 侧边栏逻辑-折叠
 const isCollapse = ref(true)
@@ -97,19 +53,7 @@ const mainStyle = computed(() => {
   };
 });
 
-//菜单逻辑匹配
-const handleSelect = (key, keyPath) => {
-  console.log(key)
-  //点击退出登录就调用退出登录
-  if (key === "2-1"){
-    User_logout().then((res)=>{
-      console.log(res)
-    })
-  }
-}
 
-
-// 侧边栏逻辑-折叠
 const toggleCollapse = () => {
   isCollapse.value = !isCollapse.value
 }
@@ -117,8 +61,9 @@ const toggleCollapse = () => {
 </script>
 
 <style lang="scss">
-.TaskPageAside {
-  transition: width 0.3s;
+.center-Page{
+  width: 100%;
+  height: 97.5vh;
 }
 
 .TaskPageMain {
@@ -126,25 +71,29 @@ const toggleCollapse = () => {
   background-color: #F5F7FA;
 }
 
+.TaskPageAside {
+  transition: width 0.3s;
+}
+
+
+
 .flex-grow {
   flex-grow: 1;
 }
 
-.center-Page{
-  width: 100%;
-  height: 97.5vh;
-}
 
-.TaskPageHeader{
-  height: 300px;
-}
+
+
 .el-menu.el-menu-demo:not(.el-menu--collapse) {
   border-bottom: none;
 }
+
 .el-menu-vertical-demo:not(.el-menu--collapse) {
   width: 200px;
   min-height: 400px;
 }
+
+//
 .el-sub-menu__icon-arrow{
   display: none  !important;
 }
